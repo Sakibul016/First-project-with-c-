9c905bb18e243dc9ee70b8b13ebe4698f72b59eb
Md Sakibul Islam <khalifasakib01611@gmail.com>
2026-05-12 12:19:24 +0600

    python output learned

--- 01-Basic-structure/Syntax.py
+++ 01-Basic-structure/Syntax.py
@@ -1,1 +1,5 @@
 print("HI Sakibul ,your journey start now.")
+#python identation is always using space before the first line ofcode blok.
+if 4>2:
+    print("hello sakib !")
+    print("Fuck you")
---
+++ 01-Basic-structure/multiple-output-in-one-line.py
@@ -0,0 +1,7 @@
+print("Hello Sakib ", end="")
+print("What is your name? ", end="")
+print("Do you love Badminton ? ")
+# remember the rule to print number . don't put number inside double quote, otherwise it will take number as string.
+print(" i am 23 years old")
+print(" I am",23 ,"years old")#we can print the same thing of 5 and 6 line in a differnt way . 
+print( 34 ,53,23)# number and string can be separated by Comma . 
