a930108504f27cf0e4f10d1011a4139e728bd483
Md Sakibul Islam <khalifasakib01611@gmail.com>
2026-05-17 23:26:28 +0600

    data type code learned

---
+++ 02-Python-variable/converting-datatype.py
@@ -0,0 +1,15 @@
+x = 3
+y = 3.3
+z = 3 + 3j # first i take three variable with three type of data , int , float, complex.
+
+a = float(x)
+b =int(y)
+c =complex(x)
+# now for converting data type , i have taken new three variable and used the converting rule . 
+print(a)
+print(b)
+print(c)
+# now i will also check data type using type() function. 
+print(type(a))
+print(type(b))
+print(type(c))
--- 02-Python-variable/getting-variable-type.py
+++ 02-Python-variable/getting-variable-type.py
@@ -4,3 +4,6 @@ z= "sakib"
 print(type(x))
 print(type(y))
 print(type(z))#we can ger the variable name in output by using type() function.
+a =dict(Name="Sakib", Age = 25)
+#Dictionary dara type insert key with value. here Name is key part, Sakib is value.
+print(a)
