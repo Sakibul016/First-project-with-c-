7d29e527170a26e25917b26be78ad707a488ef72
Md Sakibul Islam <khalifasakib01611@gmail.com>
2026-07-02 15:46:34 +0600

    string learned

--- 02-Python-variable/getting-variable-type.py
+++ 02-Python-variable/getting-variable-type.py
@@ -3,7 +3,7 @@ y= 3.4
 z= "sakib"
 print(type(x))
 print(type(y))
-print(type(z))#we can ger the variable name in output by using type() function.
+print(type(z))#we can get the variable name in output by using type() function.
 a =dict(Name="Sakib", Age = 25)
-#Dictionary dara type insert key with value. here Name is key part, Sakib is value.
+#Dictionary data type insert key with value. here Name is key part, Sakib is value.
 print(a)
--- 02-Python-variable/global-variable.py
+++ 02-Python-variable/global-variable.py
@@ -1,13 +1,13 @@
 x = 'sakib' #This is a global variable cause it outside of a function.
-def my_function():
+def first_function():
     x ='rajib'# This is a local variable cause it inside of my_function code block.  
     print("This is " + x)
-my_function()
+first_function()
 print("This is " + x)
 
 A = 23 
 def secondFunction():
-    global A # use global keywork to make varable global even when it is in function. 
+    global A # use global keywork to make varable global even when it is in a function. 
     A = 40 # global keyword even changed the value of varaiable by updating latest one .
 secondFunction()
 print(A)
