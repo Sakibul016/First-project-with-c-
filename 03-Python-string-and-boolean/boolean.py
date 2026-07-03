9b6cedbeed150fc805457c095e072da6a01079f7
Md Sakibul Islam <khalifasakib01611@gmail.com>
2026-07-03 23:19:49 +0600

    learned something

---
+++ 03-Python-string-and-boolean/boolean.py
@@ -0,0 +1,4 @@
+print(3>4)
+print(34>23)
+print(34==32)
+# if any condition becomes true , output will be true . i mean boolean expression. 
---
+++ 04-Python-Operator/Arithmatic.py
@@ -0,0 +1,9 @@
+x=22
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x%y)#modulas
+print(x/y)
+print(x**y)# it is exponentiation , it basically 22*22*22*22*22 
+print(x//y)# it is a floor division, kono vagses pabo na , only show nearest integer.
